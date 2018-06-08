4032d9b19f585e1d7c7747c9096df990fdd746c8
Dinika Saxena <dinika.saxena@cern.ch>
2018-06-08 22:10:17 +0200

    use request interceptors
    Signed-off-by: Dinika Saxena <dinika.saxena@cern.ch>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+axios.interceptors.request.use(request => {
+  console.log(request);
+  return request
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
